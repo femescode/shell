6488dd11cc42fc74b3239820c836e252c062ae3b
fengmeng <fengmeng@xiaomi.com>
2020-07-22 13:42:30 +0800

    fix: 上传

--- bash_config.sh
+++ bash_config.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
+cmd_exists() {
+    if [ -x "$(command -v "$1")" ]; then
+        return 0;
+    else
+       return 1;
+    fi
+}
 
+mkdir -p /tmp/shell-config
+cd /tmp/shell-config
+curl -s -LO https://gitee.com/fmer/shell/raw/master/.profile_rc.sh
+if [[ ! $(grep profile_rc ~/.bashrc) ]]; then
+    printf "if [ -f /etc/.profile_rc.sh ]; then\n\t. /etc/.profile_rc.sh\nfi" >> ~/.bashrc
+fi
+if cmd_exists "sudo"; then
+    sudo cp .profile_rc.sh /etc/
+else
+    cp .profile_rc.sh /etc/
+fi
+
+curl -s -LO https://gitee.com/fmer/shell/raw/master/.vimrc
+cp .vimrc ~/.vimrc
+rm -rf /tmp/shell-config
 exit 0
