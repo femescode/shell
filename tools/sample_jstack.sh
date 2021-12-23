3995a99b83bd2312a798f8cc9fd2a057b62e9fb8
fengmeng <fengmeng@xiaomi.com>
2021-12-23 13:35:44 +0800

    fix: sample

--- tools/sample_jstack.sh
+++ tools/sample_jstack.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-pid="$1"
+pid="$2"
 [[ ! "$pid" ]] && { pid=`ps h -o pid --sort=-pmem -C java|head -n1`; }
-[[ ! "$pid" ]] && { echo 'not found java process, usage: $0 pid num' >&2; exit 1; }
-jstacklog=$(mktemp)
+[[ ! "$pid" ]] && { echo "not found java process, usage: $0 num pid" >&2; exit 1; }
+jstacklog=$(mktemp -p . jstack_XXXXXXXXXX.log)
 
-num="${2:-10}"
+num="${1:-10}"
 for i in `seq $num`;do 
     jstack $pid; 
     sleep 0.2; 
 done > $jstacklog
 
-cat $jstacklog|sed -E -e 's/0x[0-9a-z]+/0x00/g' -e '/^"/ s/[0-9]+/n/g'|awk -v RS="" 'gsub(/\n/,"\\n",$0)||1' |sort|uniq -c|sort -nr|sed 's/$/\n/;s/\\n/\n/g'|less
+cat $jstacklog|sed -E -e 's/0x[0-9a-z]+/0x00/g' -e '/^"/ s/[0-9]+/n/g' \
+    |awk -v RS="" 'gsub(/\n/,"\\n",$0)||1' \
+    |sort|uniq -c|sort -nr \
+    |sed 's/$/\n/;s/\\n/\n/g' \
+    |less
 
-rm "$jstacklog"
