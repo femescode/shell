2a781a710b56f4e937ed5741ec272b44e68e412a
fengmeng <fengmeng@xiaomi.com>
2022-06-12 09:27:01 +0800

    fix: log_qps

---
+++ tools/log_qps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+grep  --line-buffered -P "$" | awk 'function gettime(l){return systime()} 
+    {
+        t=gettime($0);
+        if(!pt){pt=t}
+        if(t-pt>=1){print i/(t-pt);pt=t;i=0}
+        i++
+    }'
--- tools/log_qps_analysis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-awk -v P="$1" 'function gettime(l){return systime()} match($0,P,a){t=gettime($0);if(!pt){pt=t};if(t-pt>=1){print i/(t-pt);pt=t;i=0};i++}'
