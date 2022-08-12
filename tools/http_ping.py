b0a5a656387efe8c0bcf74d08c6643f4743441f2
fengmeng <fengmeng@xiaomi.com>
2022-08-12 14:50:06 +0800

    fix: http_piing

---
+++ tools/http_ping.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys,requests,time,json,hashlib,base64,uuid,io,os,argparse
+s = requests.Session()
+s.mount('https://', requests.adapters.HTTPAdapter(pool_connections=200, pool_maxsize=200))
+s.mount('http://', requests.adapters.HTTPAdapter(pool_connections=200, pool_maxsize=200))
+
+def http_ping(seq, args):
+    url = args.url
+    headers = {}
+    if args.header:
+        for header in args.header:
+            (key, value) = re.split(r'\s*:\s*', header.strip())
+            header[key] = value
+    start = time.time()
+    if args.request == "GET":
+        resp = s.get(url,headers=headers, timeout=6000)
+    else:
+        resp = s.post(url,data=args.data,headers=headers, timeout=6000)
+    end = time.time()
+    ret = resp.content.decode("UTF-8").replace("\r","").replace("\n","")
+    print("time=%s seq=%d cost=%.3fms" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start)), seq, (end - start) * 1000))
+
+
+parser = argparse.ArgumentParser(description='http ping test tools.')
+parser.add_argument('url')
+parser.add_argument('-X', "--request", type=str, choices=["GET", "POST"], default="GET", help='request method.')
+parser.add_argument('-d', "--data", type=str, help='request data.')
+parser.add_argument('-H', "--header", type=str, nargs='*', help='request header.')
+args = parser.parse_args()
+
+i = 0
+while True:
+    http_ping(i, args)
+    time.sleep(1)
+    i = i + 1
