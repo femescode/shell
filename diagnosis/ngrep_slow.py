38a42962085ddade86d94a5ed899ddba4ad3f4f8
fengmeng <fengmeng@xiaomi.com>
2022-08-18 15:28:07 +0800

    fix: ngrep

--- diagnosis/ngrep_slow.py
+++ diagnosis/ngrep_slow.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-import re,datetime,time,json,sys,argparse
+import re,datetime,time,json,sys,argparse,subprocess,os
+
+DEV_NULL = os.open(os.devnull, os.O_RDWR)
+
+def exists_cmd(cmd):
+    return subprocess.call(["which", cmd],stdout=DEV_NULL) == 0
 
 def cost_show(cost):
     if cost > 1:
@@ -12,9 +17,9 @@ def cost_show(cost):
         return "%.0fus" % (cost * 1000000)
     return str(cost) + 's'
 
-def trace_ngrep_slow(args):
+def trace_ngrep_slow(args, stdout):
     pre_packet_map = {}
-    for line in sys.stdin:
+    for line in stdout:
         if not line:
             continue
         line=line.strip()
@@ -31,10 +36,10 @@ def trace_ngrep_slow(args):
         payload = columns[7]
         (src_ip, src_port) = src_addr.split(":")
         (dst_ip, dst_port) = dst_addr.split(":")
-        if int(src_port) > 10000 and int(dst_port) < 10000 and (args.all or re.search(r'^(POST|GET)', payload) or re.search(r'...(select|insert|update|delete|replace)', payload, re.I)):
+        if int(src_port) > 10000 and int(dst_port) < 10000 and (not args.request_regex or re.search(args.request_regex, payload)) and (args.all or re.search(r'^(POST|GET)', payload) or re.search(r'...(select|insert|update|delete|replace)', payload, re.I)):
             # 发包，记录时间缀
             pre_packet_map[src_addr+"-"+dst_addr] = {'start': timestamp, 'req': line}
-        elif int(src_port) < 10000 and int(dst_port) > 10000 and (args.all or re.search(r'HTTP/1.[01]', payload) or re.search(r'.def.', payload, re.I)):
+        elif int(src_port) < 10000 and int(dst_port) > 10000 and (args.request_regex or args.all or re.search(r'HTTP/1.[01]', payload) or re.search(r'.def.', payload, re.I)):
             # 收包，计算时间差
             addr_pair = dst_addr+"-"+src_addr
             pre_packet = pre_packet_map.get(addr_pair)
@@ -49,9 +54,25 @@ def trace_ngrep_slow(args):
                 del pre_packet_map[addr_pair]
 
 parser = argparse.ArgumentParser(description='ngrep traffic slow response trace tools.',
-        usage="ngrep -d any -W single -s 800 -t -l  '' 'port 80'|python -u ngrep_slow.py --timeout 1000 |jq .")
+        usage="python -u ngrep_slow.py --timeout 1000 'port 80'|jq .")
+parser.add_argument('bpf_filter', default="")
+parser.add_argument('-r', '--request_regex', default=None, required=False)
 parser.add_argument("-t", "--timeout", type=int, default=1000, help='the timeout millisecond.')
 parser.add_argument("-a", "--all", action="store_true", help='trace all packet.')
 args = parser.parse_args()
 
-trace_ngrep_slow(args)
+if not exists_cmd("ngrep"):
+    sys.stderr.write("ngrep not found! \n")
+    sys.exit(1)
+
+cmd_args = ["ngrep","-d","any","-W","single","-s","800","-t","-l",""]
+if args.bpf_filter != "":
+    cmd_args.append(args.bpf_filter)
+proc = subprocess.Popen(cmd_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+try:
+    trace_ngrep_slow(args, proc.stdout)
+finally:
+    proc.stdout.close()
+    sys.stderr.write(proc.stderr.read()+"\n")
+    proc.stderr.close()
+    proc.kill()
