94ae5a9ae906e63a4db68be78d6e7b9d18e523fd
fengmeng <fengmeng@xiaomi.com>
2022-12-10 22:27:32 +0800

    fix: active socket

--- diagnosis/active_socket.sh
+++ diagnosis/active_socket.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-t="${1:-2000}"
-ss -natpeoi | sed '1!{N;s/\n//;}' | awk -v t=$t ' match($0,/lastsnd:([0-9]+)/,a) && a[1]<t'
+b="${1:-0}"
+e="${2:-2000}"
+ss -natpeoi | sed '1!{N;s/\n//;}' | awk -v b=$b -v e=$e ' match($0,/lastsnd:([0-9]+)/,a) && a[1]>b && a[1]<e'
